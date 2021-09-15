21da28fbc72c21a0b042ebd020b78480772d81a4
Artem Golovin <artgolovin96@gmail.com>
2021-09-15 00:57:42 -0600

    add testing

--- solutions/contains_duplicate.py
+++ solutions/contains_duplicate.py
@@ -7,9 +7,15 @@ LeetCode: https://leetcode.com/problems/contains-duplicate/
 def contains_duplicate(nums: list[int]) -> bool:
     seen = set([])
 
-		for num in nums:
-			if num in seen:
-				return True
-			seen.add(num)
-		
-		return False
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+
+def test_contians_duplicate():
+    assert contains_duplicate([1, 2, 3, 1])
+    assert contains_duplicate([1, 2, 3, 4]) == False
+    assert contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
