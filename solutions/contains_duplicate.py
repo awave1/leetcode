62e8eab29692c2baf40593275440a1f8996e4403
Artem Golovin <artgolovin96@gmail.com>
2021-09-15 01:10:54 -0600

    solve missing number

--- solutions/contains_duplicate.py
+++ solutions/contains_duplicate.py
@@ -1,6 +1,8 @@
 """
 217. Contains Duplicate
 LeetCode: https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 """
 
 
---
+++ solutions/missing_number.py
@@ -0,0 +1,39 @@
+"""
+268. Missing Number
+LeetCode: https://leetcode.com/problems/missing-number/
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+---
+
+Examples:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+Input: nums = [0]
+Output: 1
+Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+"""
+
+
+def missing_number(nums: list[int]) -> int:
+    range_count = len(nums) + 1
+
+    return sum(range(0, range_count)) - sum(nums)
+
+
+def test_missing_number():
+    assert missing_number([3, 0, 1]) == 2
+    assert missing_number([0, 1]) == 2
+    assert missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
+    assert missing_number([0]) == 1
