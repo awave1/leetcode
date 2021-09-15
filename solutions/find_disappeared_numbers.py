0ed8cbbdc886dfcc22534ec6c2acf880414bdbce
awave1 <artgolovin96@gmail.com>
2021-09-15 11:21:22 -0600

    wip

--- solutions/find_disappeared_numbers.py
+++ solutions/find_disappeared_numbers.py
@@ -9,12 +9,24 @@ Given an array nums of n integers where nums[i] is in the range [1, n], return a
 
 def find_disappeared_numbers(nums: list[int]) -> list[int]:
 
-    full_range = set(list(range(1, len(nums) + 1)))
+    n = len(nums)
+
+    full_range = set(list(range(1, n)))
     # solution using a filter
-    # list(filter(lambda x: x not in nums, list(range(1, len(nums) + 1))))
+    # list(filter(lambda x: x not in nums, list(range(1, n))))
     return list(full_range.difference(set(nums)))
+
+
+def find_disappeared_numbers_improved(nums: list[int]) -> list[int]:
+    """
+    Cyclic sort solution
+    """
+
+    n = len(nums)
 
 
 def test_find_disappeared_numbers():
     assert find_disappeared_numbers([4, 3, 2, 7, 8, 2, 3, 1]) == [5, 6]
     assert find_disappeared_numbers([1, 1]) == [2]
+    assert find_disappeared_numbers_improved([4, 3, 2, 7, 8, 2, 3, 1]) == [5, 6]
+    assert find_disappeared_numbers_improved([1, 1]) == [2]
