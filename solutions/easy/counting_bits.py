700e0ff4c0a679e70f022b69c86f35e546db9a5f
Artem Golovin <artgolovin96@gmail.com>
2021-09-21 14:14:09 -0600

    add dp solution for counting bits

--- solutions/easy/counting_bits.py
+++ solutions/easy/counting_bits.py
@@ -25,7 +25,7 @@ Explanation:
 """
 
 
-def counting_bits(n: int) -> list[int]:
+def counting_bits_brute_force(n: int) -> list[int]:
     bits = []
     for i in range(0, n + 1):
         bits.append(bin(i).count("1"))
@@ -33,9 +33,35 @@ def counting_bits(n: int) -> list[int]:
     return bits
 
 
-def test_counting_bits():
-    assert counting_bits(2) == [0, 1, 1]
-    assert counting_bits(5) == [0, 1, 1, 2, 1, 2]
+def counting_bits_dp(n: int) -> list[int]:
+    """
+    Strategy:
+    0  0 0 0 0
+    1  0 0 0 1
+    2  0 0 1 0
+    3  0 0 1 1
+    4  0 1 0 0
+    5  0 1 0 1
+    6  0 1 1 0
+    7  0 1 1 1
+
+    memo[x] = memo[x >> 1] + x % 2
+    """
+    memo_bits = [0] * (n + 1)
+    for i in range(0, n + 1):
+        # i >> 1 = i / 2 (approx)
+        prev_value = i >> 1
+        even_bit = i % 2
+        memo_bits[i] = memo_bits[prev_value] + even_bit
+
+    return memo_bits[: n + 1]
 
 
-counting_bits(2)
+def test_counting_bits_brute_force():
+    assert counting_bits_brute_force(2) == [0, 1, 1]
+    assert counting_bits_brute_force(5) == [0, 1, 1, 2, 1, 2]
+
+
+def test_counting_bits_dp():
+    assert counting_bits_dp(2) == [0, 1, 1]
+    assert counting_bits_dp(5) == [0, 1, 1, 2, 1, 2]
