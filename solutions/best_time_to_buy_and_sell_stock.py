58660588280a46b1e7267c0c835001540d2d1983
awave1 <artgolovin96@gmail.com>
2021-09-20 22:12:57 -0600

    solve max profit via brute force (O(n^2))

---
+++ solutions/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,41 @@
+"""
+121. Best Time to Buy and Sell Stock
+src: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+---
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+"""
+
+
+def max_profit_brute_force(prices: list[int]) -> int:
+    profit = -float("inf")
+
+    for i, price1 in enumerate(prices):
+        for price2 in prices[i + 1 :]:
+            potential_profit = price2 - price1
+            if potential_profit > profit:
+                profit = potential_profit
+
+    return 0 if profit < 0 else profit
+
+
+def test_max_profit_brute_force():
+    assert max_profit_brute_force([7, 1, 5, 3, 6, 4]) == 5
+    assert max_profit_brute_force([7, 6, 4, 3, 1]) == 0
+    assert max_profit_brute_force([2, 4, 1]) == 2
