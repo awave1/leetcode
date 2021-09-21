e5e7c8fb2e0b68e2b2b147ac54bcb00adbf194c9
Artem Golovin <artgolovin96@gmail.com>
2021-09-21 14:54:20 -0600

    solve range sum

--- solutions/easy/range_sum_query.py
+++ solutions/easy/range_sum_query.py
@@ -4,7 +4,8 @@ src: https://leetcode.com/problems/range-sum-query-immutable/
 
 Given an integer array nums, handle multiple queries of the following type:
 
-Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+1. Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+
 Implement the NumArray class:
 
 - NumArray(int[] nums) Initializes the object with the integer array nums.
@@ -24,3 +25,27 @@ numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
 numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
 numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 """
+
+
+class NumArray:
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+
+    def sum_range_using_sum(self, left: int, right: int) -> int:
+        return sum(self.nums[left : right + 1])
+
+    def sum_range(self, left: int, right: int) -> int:
+        sum_result = 0
+
+        for i in range(left, right + 1):
+            print(self.nums[i])
+            sum_result += self.nums[i]
+
+        return sum_result
+
+
+def test_sum_range():
+    num_array = NumArray([-2, 0, 3, -5, 2, -1])
+    assert num_array.sum_range(left=0, right=2) == 1
+    assert num_array.sum_range(left=2, right=5) == -1
+    assert num_array.sum_range(left=0, right=5) == -3
