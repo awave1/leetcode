f990b32bc5086afa018653cf1a400206ddf6d989
awave1 <artgolovin96@gmail.com>
2021-09-25 14:52:20 -0600

    improve range sum query

--- solutions/easy/range_sum_query.py
+++ solutions/easy/range_sum_query.py
@@ -30,11 +30,25 @@ numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 class NumArray:
     def __init__(self, nums: list[int]):
         self.nums = nums
+        self.sums = []
+
+        sum = 0
+        for n in nums:
+            sum += n
+            self.sums.append(sum)
+
+        print(self.sums)
 
     def sum_range_using_sum(self, left: int, right: int) -> int:
         return sum(self.nums[left : right + 1])
 
     def sum_range(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.sums[right]
+
+        return self.sums[right] - self.sums[left - 1]
+
+    def sum_range_loop(self, left: int, right: int) -> int:
         sum_result = 0
 
         for i in range(left, right + 1):
